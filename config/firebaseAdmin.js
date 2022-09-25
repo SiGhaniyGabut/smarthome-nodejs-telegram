c030b0e3cb22d8a69558169af099e4806e508a12
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-09-26 00:33:41 +0700

    Change Service Account Config

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -5,9 +5,9 @@ const serviceAccount = {
     private_key: process.env.FIREBASE_PRIVATE_KEY,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
 }
 
