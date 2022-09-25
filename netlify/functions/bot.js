f63503454f915a641e785b74385673b81f5be73e
GhaniyKie <abdulhakimghaniy@outlook.co.id>
2022-09-26 03:47:55 +0700

    Add bot functionality

--- netlify/functions/bot.js
+++ netlify/functions/bot.js
@@ -4,25 +4,24 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 // Get the relays from the database
 const db = app.firestore();
-const docRef = db.collection('relays')
+const relaysCollection = db.collection('relays')
 
 bot.start((ctx) => ctx.reply('Welcome!, I am SmartHome Bot!. Send /relays to see the relays lists'));
 
 bot.command("relays", async ctx => {
-    const snapshot = await docRef.get();
-    if (snapshot.empty) {
+    const relays = await relaysCollection.get();
+    if (relays.empty) {
         console.log('No matching documents.');
         return;
     }
 
-    ctx.reply(snapshot.docs.map(doc => doc.data()));
-    snapshot.docs.map(doc => {
+    relays.docs.map(relay => {
         try {
             ctx.reply(
-                doc.data().name,
+                relay.data().name,
                 Markup.inlineKeyboard([
-                    Markup.button.callback("ON", doc.data().id + "_on"),
-                    Markup.button.callback("OFF", doc.data().id + "_off"),
+                    Markup.button.callback("ON", relay.data().id + "_on"),
+                    Markup.button.callback("OFF", relay.data().id + "_off"),
                 ]),
             );
         }
@@ -32,6 +31,34 @@ bot.command("relays", async ctx => {
         }
     })
 });
+
+bot.action(/.+/, ctx => {
+    setRelayStatus(ctx)
+});
+
+const setRelayStatus = async (context) => {
+    const relays = await relaysCollection.get();
+    let id = context.callbackQuery.data.match(/(\d+)/i)[0];
+    let status = context.callbackQuery.data.match(/on|off/i)[0];
+    let relayId = `relay_${id}`;
+
+    relays.docs.map(relay => {
+        if (relay.data().id === relayId) {
+            if (relay.data().status === status) {
+                context.answerCbQuery(`${relay.data().name} sudah ${status == 'on' ? 'dinyalakan' : 'dimatikan'}`);
+            }
+
+            if (relay.data().status !== status) {
+                context.answerCbQuery(`Oke, ${context.callbackQuery.message.text} ${status == 'on' ? 'dinyalakan' : 'dimatikan'}`);
+                relay.data().status = status;
+            }
+        }
+    })
+
+    relaysCollection.doc(relayId).update({
+        status: status
+    })
+}
 
 exports.handler = async (event, context) => {
     try {
